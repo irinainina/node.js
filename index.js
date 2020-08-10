a4d24dccade07db1895783999a67754e85829ce4
Irina <irbris@mail.ru>
2020-08-10 17:40:44 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,33 @@
+let request = require("request");
+let readline = require("readline");
+let colors = require("colors");
+
+function requestWords() {
+  let requestWords = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+    terminal: false,
+  });
+
+  requestWords.question(
+    "Введите слово или фразу на русском языке - ".red,
+    (answer) => {
+      requestWords.close();
+      let encodeWordUri = encodeURI(answer);
+      request(
+        "https://translate.yandex.net/api/v1.5/tr.json/translate?key=trnsl.1.1.20200322T155651Z.de98a60e6a99185e.089aea4237b51c6db082c966f27a7895cd1e8b44&text=" +
+          encodeWordUri +
+          "&lang=ru-en",
+        (err, res, body) => {
+          if (!err && res.statusCode == 200) {
+            let obj = JSON.parse(body);
+            console.log("**************ПЕРЕВОД**************");
+            console.log(obj.text[0]);
+          }
+        }
+      );
+    }
+  );
+}
+console.log("Русско-Английский переводчик".green);
+requestWords();
